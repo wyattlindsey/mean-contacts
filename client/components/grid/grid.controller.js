3637c2a9b08bc2f373f51b817133a6678da8c0d0
Wyatt Lindsey <wyatt@newrelic.com>
2014-10-13 23:22:13 -0700

    break modal fields into two columns

--- client/components/grid/grid.controller.js
+++ client/components/grid/grid.controller.js
@@ -22,6 +22,7 @@ angular.module('contactsApp')
           gridApi.selection.on.rowSelectionChanged($scope, function(row){
 
             $scope.selectedItems = gridApi.selection.getSelectedRows();
+            console.log(row);
 
             // logic for updating data in contact details view
             if ($scope.selectedItems.length === 1) {
@@ -30,6 +31,13 @@ angular.module('contactsApp')
             } else {
               //clear out data and pull in defaults when more than one row is selected
               $scope.singleSelectedItem = null;
+            }
+
+            // deselecting row when multiple items are selected but multiSelect is false
+            if ($scope.selectedItems.length > 1 && !$scope.gridOptions.multiSelect && !row.isSelected) {
+              $scope.selectedItems = [];
+              $scope.singleSelectedItem = null;
+              $scope.selectSingleRow(row.entity);
             }
 
             if ($scope.editInProgress) {
@@ -118,7 +126,8 @@ angular.module('contactsApp')
         ];
 
       $scope.selectSingleRow = function(rowEntity) {
-        $scope.selectedItems = rowEntity;
+        $scope.gridApi.selection.clearSelectedRows();
+        $scope.selectedItems[0] = rowEntity;
         $scope.singleSelectedItem = rowEntity;
         $scope.gridApi.selection.selectRow(rowEntity);
       };
@@ -148,8 +157,7 @@ angular.module('contactsApp')
           return deferred.promise;
         };
 
-        var promise = deleteLoop();
-        promise.then(function() {
+        deleteLoop().then(function() {
           $scope.getContacts();
           $scope.selectedItems = [];
           $scope.singleSelectedItem = null;
@@ -162,11 +170,13 @@ angular.module('contactsApp')
       $('body').keydown(function (e) {
         if ((e.keyCode === 16 || e.keyCode === 17 || e.metaKey || e.keyCode === 224 || e.keyCode === 91 || e.keyCode === 93) && !$scope.multiSelect) {
           $scope.gridApi.selection.setMultiSelect(true);
+          $scope.gridOptions.multiSelect = true;
           $scope.allowCellEdit = false;
         }
       }).keyup(function (e) {
         if (e.keyCode === 16 || e.keyCode === 17 || e.metaKey || e.keyCode === 224 || e.keyCode === 91 || e.keyCode === 93) {
           $scope.gridApi.selection.setMultiSelect(false);
+          $scope.gridOptions.multiSelect = false;
           $scope.allowCellEdit = true;
         }
       });
@@ -175,7 +185,25 @@ angular.module('contactsApp')
       //where keyup never happens and multi-select stays true
       $(window).blur(function() {
         $scope.gridApi.selection.setMultiSelect(false);
+        $scope.gridOptions.multiSelect = false;
         $scope.allowCellEdit = true;
+      });
+
+      // arrow keys move selection up and down
+      $(document).keydown(function(e) {
+        switch(e.which) {
+          // I don't see a way in the public API to get the sorted array of rows
+          case 38: // up
+
+            break;
+
+          case 40: // down
+
+            break;
+
+          default: return; // exit this handler for other keys
+        }
+        e.preventDefault(); // prevent the default action (scroll / move caret)
       });
 
   });
